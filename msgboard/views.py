7115edde785d0a519e3e718c8879e58a437bf62a
OmerCS8 <omercs8@gmail.com>
2022-02-13 17:45:23 +0200

    several bug fixes to the msgboard

--- msgboard/views.py
+++ msgboard/views.py
@@ -4,15 +4,16 @@ from .forms import MessageForm
 
 
 def board(request):
-	messages = Message.objects.order_by('-date')
-	if request.method == "POST":
-		form = MessageForm(request.POST)
-		if form.is_valid():
-			form.save()
-			return redirect('board')
-	else:
-		form = MessageForm()
-	return render(request, 'msgboard/board.html', {
-		'messages': messages,
-		'form': form,
-	})
+    messages = Message.objects.order_by('-date')
+    if request.method == "POST":
+        form = MessageForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('board')
+    else:
+        form = MessageForm()
+    return render(request, 'msgboard/board.html', {
+    'messages': messages,
+    'form': form,
+    })
+	
